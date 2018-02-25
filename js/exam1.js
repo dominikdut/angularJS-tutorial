e6950ee29e1397fa573a7fb87a68ed3b1e34e40a
Dominik Dutkiewicz <dominikdut@gmail.com>
2018-02-25 21:08:48 +0100

    Made function adding to number

--- js/exam1.js
+++ js/exam1.js
@@ -1,7 +1,13 @@
-
 var app1 = angular.module('app1', []);
 
 app1.controller('ctr1', function ($scope) {
 
+  $scope.first = 1;
+  $scope.second = 1;
+
+
+  $scope.updateValue = function () {
+    $scope.calculation = $scope.first + '+' + $scope.second + " = " + (+$scope.first + $scope.second);
+  }
 
 });
