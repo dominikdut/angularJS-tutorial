403ae0aca759b4283f2eea90b8b4b135eb9dd93e
Dominik Dutkiewicz <dominikdut@gmail.com>
2018-02-25 22:39:37 +0100

    Multiple views fix

--- js/exam2.js
+++ js/exam2.js
@@ -2,7 +2,7 @@ var app2 = angular.module('app2', []);
 
 app2.controller('ctrl1', function ($scope) {
 
-  $scope.randomNum1 = Math.floor((Math.random() * 10) +1)
+  $scope.randomNum1 = Math.floor((Math.random() * 10) +1);
   $scope.randomNum2 = Math.floor((Math.random() * 10) +1)
 
 });
