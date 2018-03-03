6991207683b64927a922d5e290358bef8d77c826
Dominik Dutkiewicz <dominikdut@gmail.com>
2018-03-03 23:40:40 +0100

    Added function showing ordered or unordered list

--- js/exam3.js
+++ js/exam3.js
@@ -12,4 +12,8 @@ app3.controller('gListCtrl', function ($scope) {
     {item: "Hummus", purchased: false}
   ];
 
+  $scope.getList = function() {
+    return $scope.showList ? "ulgrocerylist.html" : "grocerylist"
+  }
+
 });
